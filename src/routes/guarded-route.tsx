9b53a9aa5b683ee88c56b4494035c3a10ccb25ea
mahdi <mxhdiqaim@gmail.com>
2025-09-23 18:31:07 +0100

    feat: add BlankLayout and CustomCard components; implement ServerDown page with error handling

--- src/routes/guarded-route.tsx
+++ src/routes/guarded-route.tsx
@@ -1,31 +1,33 @@
 import { memo, type ReactNode } from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import { useOAuth, useStatus } from "@openfort/react";
+import Spinner from "@/components/ui/spinner.tsx";
+import ServerDown from "@/pages/feedbacks/server-down.tsx";
 
-// import ServerDown from "@/pages/feedbacks/server-down";
-
-type GuardProps = {
+type Props = {
     authGuard: boolean;
     children: ReactNode;
 };
 
 // This is your GuardedRoute component that checks authentication status
-const GuardedRoute = memo(function GuardedRoute({ children, authGuard }: GuardProps) {
-    // const { isLoading, isAuthenticated /* isServerOk */ } = useAuthStatus();
+const GuardedRoute = memo(({ children, authGuard }: Props) => {
+    const { isAuthenticated } = useStatus();
+    const { isSuccess: isServerOk, isLoading } = useOAuth();
     const location = useLocation();
 
     // Show loading spinner if still checking status
-    // if (isLoading) return <Spinner />;
+    if (isLoading) return <Spinner />;
 
     // // Show server down page if the server isn't responding
-    // if (!isServerOk) return <ServerDown />;
+    if (!isServerOk) return <ServerDown />;
 
-    // If the route requires auth and user is NOT authenticated, redirect to log in
-    if (authGuard /* && !isAuthenticated */) {
+    // If the route requires auth and the user is NOT authenticated, redirect to log in
+    if (authGuard && !isAuthenticated) {
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
-    // If the route requires auth and user is authenticated, show the page
-    if (authGuard /* && isAuthenticated */) {
+    // If the route requires auth and the user is authenticated, show the page
+    if (authGuard && isAuthenticated) {
         return <>{children}</>;
     }
 
